e73eb7006b42910d38f67d3dd3afa2a99d1daa99
woolfenr <rosswoolfenden@gmail.com>
2021-03-24 13:19:59 +0000

    added route to index

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ const config = require('./config');
 const logging = require('./logging/WinstonLogging');
 const cors = require('@koa/cors');
 const userroute = require('./routes/users');
+const movieroute = require('./routes/movies');
 const app =  new Koa();
 const port = config.info.port;
 
 const log =  logging.createLogger('Server');
 app.use(cors());
 app.use(userroute.routes());
+app.use(movieroute.routes());
 
 app.listen(port, () => log.info(`Public libary Api is now running on port ${port}`));
---
+++ api/routes/movies.js
@@ -0,0 +1,41 @@
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const logging = require('../logging/WinstonLogging');
+// const model = require('../models/movies');
+const auth = require('../controllers/auth');
+const log = logging.createLogger('Movie Routes');
+const router = Router({prefix: '/api/movies'});
+
+
+router.post('/addmovie', bodyParser(), auth, addmovie);
+
+
+/**
+ * Route to fir users to add movie to their favirotes
+ * @param {Object} ctx - The koa Request/response object 
+ */
+ async function addmovie(ctx) {
+    const username = ctx.state.user.username;
+    const movie = ctx.request.body.movie;
+    // let movie = '123'
+    try {  
+        const succsess = await model.addFaveMovie(username, movie);
+        let res;
+        if(succsess.affectedRows) {
+            log.info("Succsefully added movie to favirotes");
+            ctx.status = 201;
+            res = {Sucsess: true, message: `succsefully added book ${succsess.insertId}`};
+        } else {
+            log.error("Failed to add new user");
+            ctx.status = 409;
+            res = {Sucsess: false, message: 'Failed to add to favirotes'};   
+        }
+        ctx.body= res;
+    } catch (e) {
+        log.error(e.toString());
+        ctx.status = 500;
+        ctx.body = {Error: 'Server Error, please try again'};
+    }
+}
+
+module.exports = router;
