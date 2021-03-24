b83846e2e92520e7423b1b36793f672e24942a8a
woolfenr <rosswoolfenden@gmail.com>
2021-03-24 12:21:31 +0000

    added route to register

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,6 +5,7 @@ const router = Router({prefix: '/api/users'});
 const log = logging.createLogger('Users Routes');
 
 router.post('/', welcome);
+router.post('/register', bodyParser(), register);
 
 
 /**
@@ -23,5 +24,31 @@ router.post('/', welcome);
     }
 }
 
+
+/**
+ * Route to register new users
+ * @param {Object} ctx - The koa Request/response object  
+ */
+ async function register(ctx) {
+    const body = ctx.request.body;
+    try {
+        const succsess = await model.registerUser(body);
+        let res;
+        if(succsess.affectedRows) {
+            log.info("Added new user");
+            res = {Sucsess: true, message: `succsefully registered user ${succsess.insertId}`};
+            ctx.status = 201;
+        } else {
+            log.error("Failed to add new user");
+            res = {Sucsess: false, message: 'Failed to register user'};
+            ctx.status = 409
+        }
+        ctx.body = res
+    } catch (e) {
+        log.error(e.toString());
+        ctx.status = 500;
+        ctx.body = {Error: 'Server Error, please try again'};
+    }
+}
 
 module.exports = router;
