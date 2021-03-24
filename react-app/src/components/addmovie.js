8b62d57e84a83a7b8cb89e70c86989cf40ecf92e
woolfenr <rosswoolfenden@gmail.com>
2021-03-24 17:27:35 +0000

    added search to state and updates

--- react-app/src/components/addmovie.js
+++ react-app/src/components/addmovie.js
@@ -1,27 +1,43 @@
 import '../App.css';
 
 import{ PageHeader, Input} from 'antd';
+import { useState } from 'react';
 
 
 const {Search} = Input;
 
+
+
 function AddMovies() {
-    return (
-        <>
-          <div className="site-layout-content">
-            <div style={{ padding: '2% 20%' }}>
-              <Search placeholder="input search text"
-                allowClear
-                enterButton="Search"
-                size="large"
-                onSearch={null}/>
-              <PageHeader className="site-page-header"
-                title="Add to my Favirote Movies"
-                subTitle="Welcome to my favirote movies"/>
-            </div>  
-          </div>
-        </>  
-      );
+
+  const [input, setInput] =  useState();
+
+  async function search() {
+    console.log("We are searching");
+  }
+  
+  const inputChange = (e) => {
+    setInput(e.target.value);
+    console.log(input)
+  }
+
+  return (
+      <>
+        <div className="site-layout-content">
+          <div style={{ padding: '2% 20%' }}>
+            <Search placeholder="Search for a movie! "
+              allowClear
+              enterButton="Search"
+              size="large"
+              onChange={inputChange}
+              onSearch={search}/>
+            <PageHeader className="site-page-header"
+              title="Add to my Favirote Movies"
+              subTitle="Welcome to my favirote movies"/>
+          </div>  
+        </div>
+      </>  
+    );
 }
 
 
