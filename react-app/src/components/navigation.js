c90849b6a99e83d30d04102eedddb7cce1447f9b
woolfenr <rosswoolfenden@gmail.com>
2021-03-24 14:19:11 +0000

    created nav bar

---
+++ react-app/src/components/navigation.js
@@ -0,0 +1,16 @@
+import { Menu } from 'antd';
+import { Link } from "react-router-dom";
+
+function Navbar(props) {
+  return (
+    <>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1"> <Link to="/">My list </Link></Menu.Item>
+        <Menu.Item key="2"> <Link to="/add"> Add to list </Link></Menu.Item>    
+      </Menu>
+    </>
+  );
+}
+
+export default Navbar;
