c324285473a7c0421c33188883f7679df4fd0fbb
woolfenr <rosswoolfenden@gmail.com>
2021-03-24 14:28:30 +0000

    retreive book infomation from api

--- react-app/src/components/home.js
+++ react-app/src/components/home.js
@@ -1,28 +1,46 @@
 import '../App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import{ PageHeader, Input} from 'antd';
 
+function Home(props) {
+  const [movies, setMovies] = useState();
+  const auth = {
+    username: 'user',
+    password: 'qwerty'
+  };
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'http://localhost:8080/api/movies/mymovies',
+      headers: {
+        "Authorization": "Basic " + btoa(auth.username + ":" + auth.password),
+      }
+    }).then(res => {
+      // What to do we with the resposne
+      setMovies((res.data.favourite_movies).split(','))
+      // console.log( movies)
+    }).catch(err => {
+      // Todo - better handling of errors
+      console.log(err);
+    });
+  }, []);
 
-const {Search} = Input;
+  // faveArrays = (favirotes.favourite_movies).split(',');
+  return (
+    <>
+      <div className="site-layout-content">
+        <div className="page-header">
+          <h1> Your favirotes! </h1>
+        </div>
 
-function Home() {
-    return (
-        <>
-          <div className="site-layout-content">
-            <div style={{ padding: '2% 20%' }}>
-              <Search placeholder="input search text"
-                allowClear
-                enterButton="Search"
-                size="large"
-                onSearch={null}/>
-              <PageHeader className="site-page-header"
-                title="Welcome to my Favirote Movies"
-                subTitle="Welcome to my favirote movies"/>
-            </div>  
-          </div>
-        </>  
-      );
+        <div className="cardlist">
+          {/*  */}
+          <p> Movies go here </p>
+        </div>  
+      </div>
+    </>  
+  );
 }
-
 
 export default Home;
