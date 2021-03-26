fcd72100c0f0839258d5c3f202aeff972d1fbc6c
woolfenr <rosswoolfenden@gmail.com>
2021-03-26 00:50:31 +0000

    final commit

--- api/config.js
+++ api/config.js
@@ -3,10 +3,10 @@ exports.info = {
 }
 
 exports.mariadb = {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '',
     port: process.env.DB_PORT || '3306',
     user: process.env.DB_USER || 'api',
-    password: process.env.DB_PW || 'password',
+    password: process.env.DB_PW || '',
     database: process.env.DB_NAME || 'lumiadb',
     connectionLimit: 5
   }
